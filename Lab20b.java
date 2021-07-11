3dd94c9ac6a7b7b3afa9ec14fcce2f5b0353f5f5
_Kevin_Zhang_ <49049813+Dragolla@users.noreply.github.com>
2021-07-10 22:05:58 -0700

    lab 20a

--- Lab20b.java
+++ Lab20b.java
@@ -3,7 +3,11 @@ import java.util.*;
 /*
    Lab 20b
    Point version: aiming for 100
-
+   Description:
+      This lab is based off of moving elements within an array. First, given a randomly placed array, the program will sort
+   the given array using quick sort. Then, after sorting it into ascending order, the program will rearrange the program
+   based off the pattern of biggest number, smallest number, second biggest, second smallest...etc 
+      
 */
 public class Lab20b
 {
@@ -11,9 +15,18 @@ public class Lab20b
 		
       int[] array = {7, 8, 1, 3, 2, 5, 4, 9, 11, 10, 14, 12, 13};
       int n = array.length;
-      QuickSort(array, 0, n - 1 );
-	}
+      Program.QuickSort(array, 0, n - 1 );
+      System.out.println("Sorting array using quicksort... ");
+      System.out.println(Arrays.toString(array));
+      Program.Rearrange(array);
+      System.out.println("Rearranging array...");
+      System.out.println(Arrays.toString(array));
+      
+    	}
+     
    
+}
+class Program{
    //depends on the array being sorted in ascending order
    public static void Rearrange(int[] arr){
       int[] temp = arr.clone(); //clone the original array
@@ -64,8 +77,10 @@ public class Lab20b
       
       }
       //swap pivot with where i is, pivot is now in the correct position
-      swap(array, i + 1, pivot);
-      return i + 1; //returns the partition  index for QuickSort
+        int temp = array[i+1];
+        array[i+1] = array[end];
+        array[end] = temp;      
+        return i + 1; //returns the partition  index for QuickSort
    
    }
 
@@ -79,16 +94,45 @@ public class Lab20b
          
          QuickSort(array, start, PartIndex - 1); //left side
          
-         QuickSort(array, PartIndex - 1, end); //right side
+         QuickSort(array, PartIndex + 1, end); //right side
          
       }
    
    } 
-   
+   //equilibrium index is when the sum of every element on the left equals the sum of every element on the right
+   //we can find the sum of the left array by subtracting the total sum by the current element minus the current indexed
+   public static String FindEquilibrium(int[] array){
+      //{1,2,3,4,4,5}
+      int total = 0;
       
+      for(int i : array){
+         total += i;
+      
+      }
+      Arraylist<Integer> index = new Arraylist<Integer>();
+      int rightSum = 0;
+      for(int i = array.length - 1; i >= 0; i--){
+         //if sum from right array equals sum from left array
+         if(rightSum == total - array[i] + rightSum){
+            index.add(i);
+         }
+         
+         //add current array element to the right sum
+         rightSum += array[i];
+      
+      }
+      int[] indexArr = new int[index.size()];
+      int count = 0;
+      for(int i : index){
+         index[count] = i;
+         count++;
+         
+      }
+      return Arrays.toString(indexArray);
+   
+   }
+   
+}   
    
    
    
-   
-   
-}
