b97f4165c7d52dd85380c111db55d7dcc2b7e158
_Kevin_Zhang_ <49049813+Dragolla@users.noreply.github.com>
2021-07-19 15:18:44 -0700

    Update Lab21b.java

--- Lab21b.java
+++ Lab21b.java
@@ -1,7 +1,43 @@
 class Lab21b{
+
    public static void main(String[] args){
+      
+
+
    
    
    }
-
+   
+   public static int FindSum(int[] array, int k){
+      
+      //the sum to find
+      int sum = 0;
+      
+      //stores the length of elements
+      int length = Integer.MAX_VALUE;
+      
+      //the start of the array
+      int start = 0;
+      
+      for(int end = 0; start < array.length; end++){
+         //add the element value to the sum
+         sum += array[end];
+         
+         //if the sum exceeds the given value of k
+         while(sum > k && start <= right){
+            
+            //set length to new value if there is a minimum value of length found
+            length = Integer.min(length, end - start + 1);
+            
+            //remove elements from the start of the array
+            sum -= array[start];
+            start++;
+         
+         } 
+      
+      }
+      return len;
+   }
+   
+   
 }
