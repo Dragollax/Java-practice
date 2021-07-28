9dd143fad5d751e06ec5755e4b9b5d7c284e7745
_Kevin_Zhang_ <49049813+Dragolla@users.noreply.github.com>
2021-07-27 21:42:54 -0700

    Create Final.java

---
+++ Final.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+/*
+   Lab 22 final
+   Point version: Aimed for highest possible
+*/
+
+
+public class Final
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
